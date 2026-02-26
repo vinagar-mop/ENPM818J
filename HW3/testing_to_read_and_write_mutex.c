0639139d7fae0e8c744b72401e027b3564e0c93c
Vinay Agarwal <agarvini@protonmail.com>
2026-02-26 14:06:54 -0500

    First draft of hw3

--- HW3/testing_to_read_and_write_mutex.c
+++ HW3/testing_to_read_and_write_mutex.c
@@ -2,6 +2,9 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define LENGHT_OF_BUFFER 40
 
 int buffer = 0;
 
@@ -24,14 +27,62 @@ sem_t sem_sig;
 void *prints_when_released(void *i)
 {
     // Prints key released but waits when it is pressed
-    
-    
+    FILE *file_read;
+    char *buffer = NULL;
+    size_t bufsize = LENGHT_OF_BUFFER;
+    char *sub_str = "release";
+    const char delim[] = " ";
+    char *token;
+
+    pthread_mutex_lock(&mutex_lock);
+    buffer = (char*) malloc(LENGHT_OF_BUFFER * sizeof(char));
+    file_read = fopen("key_pressed.txt", "r");
+
+    if (buffer == NULL)
+    {
+        exit(1);
+    }
+
+    while(fgets(buffer, bufsize, file_read) != NULL)
+    {
+        if(strstr(buffer, sub_str) != NULL)
+        {
+            printf("%s",buffer);
+        }
+    }
+    fclose(file_read);
+    pthread_mutex_unlock(&mutex_lock);
+
 }
 
 void *prints_when_pressed(void *i)
 {
     // Print when the key is pressed but wait when it is releases
+    FILE *file_read;
+    char *buffer = NULL;
+    size_t bufsize = LENGHT_OF_BUFFER;
+    char *sub_str = "press";
+    const char delim[] = " ";
+    char *token;
 
+    pthread_mutex_lock(&mutex_lock);
+    buffer = (char*) malloc(LENGHT_OF_BUFFER * sizeof(char));
+    file_read = fopen("key_pressed.txt", "r");
+
+    if (buffer == NULL)
+    {
+        exit(1);
+    }
+
+    while(fgets(buffer, bufsize, file_read) != NULL)
+    {
+        if(strstr(buffer, sub_str) != NULL)
+        {
+            printf("%s",buffer);
+        }
+    }
+    fclose(file_read);
+    pthread_mutex_unlock(&mutex_lock);
 }
 
 
@@ -40,27 +91,27 @@ int main()
     int counter = 0;
     pthread_t thread1, thread2;
 
-    // pthread_mutex_init(&locker, NULL);
-    // sem_init(&sem_sig, 0, 1);
-    // pthread_mutex_init(&mutex_lock, NULL);
-
-    // pthread_create(&thread1, NULL, prints_when_released, (void*)&counter);
-    // pthread_create(&thread2, NULL, prints_when_pressed, (void*)&counter);
-
-    // pthread_join(thread1, NULL);
-    // pthread_join(thread2, NULL);
-
-    // sem_destroy(&sem_sig);
-    // pthread_mutex_destroy(&mutex_lock);
-    //pthread_mutex_destroy(&locker);
+    // tell the user to input system
+    printf("Start Typing the input\n");
 
     // Testing key presses
-    if (system("sudo showkey > key_pressed.txt ") != 0)
+    if (system("sudo showkey > key_pressed.txt ") == 0)
     {
-        printf("command for keypress failed.");
+        printf("command for keypress failed");
     }
+    // pthread_mutex_init(&locker, NULL);
+    // sem_init(&sem_sig, 0, 1);
+    pthread_mutex_init(&mutex_lock, NULL);
 
-    // S
+    pthread_create(&thread1, NULL, prints_when_released, (void*)&counter);
+    pthread_create(&thread2, NULL, prints_when_pressed, (void*)&counter);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    // sem_destroy(&sem_sig);
+    pthread_mutex_destroy(&mutex_lock);
+    //pthread_mutex_destroy(&locker);
 
     return 0;
 }
